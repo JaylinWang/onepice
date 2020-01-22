2101a9447c048ccfb035bb8d2ab1cee9cdb28f3a
继林 <jaylin.wjl@alibaba-inc.com>
2020-01-23 00:01:43 +0800

    feat: 升级gulp，替换sass为less

--- gulpfile.js
+++ gulpfile.js
@@ -1,135 +1,56 @@
-'use strict';
-
-var gulp = require('gulp');
-var plumber = require('gulp-plumber');
-var sass = require('gulp-sass');
-var header = require('gulp-header');
-var concat = require('gulp-concat');
-var addSrc = require('gulp-add-src');
-var uglify = require('gulp-uglify');
-var prefix = require('gulp-autoprefixer');
-var zip = require('gulp-zip');
+const { series, src, dest } = require('gulp');
+const less = require('gulp-less');
+const concat = require('gulp-concat');
+const uglify = require('gulp-uglify');
 var pkg = require('./package.json');
-var browserSync = require('browser-sync').create();
 
-var PORT = {
-  GHOST: '2368',
-  BROWSERSYNC: '3000'
+const distPath = {
+  name: pkg.name,
+  css: 'assets/css',
+  js: 'assets/js',
+  image: 'assets/images',
+  font: 'assets/fonts',
+  vendor: 'assets/vendors',
+}
 
-};
+const scrPath = {
+  less: {
+    main: 'src/less/index.less'
+  },
+  js: {
+    main: ['src/js/index.js']
+  },
+  font: {
+    main: 'src/fonts/**/**'
+  },
+  vendor: {
+    main: 'src/vendors/**/**'
+  }
+}
 
-//# Files
-//
-var dist = (function () {
-  return {
-    name: pkg.name,
-    css: 'assets/css',
-    js: 'assets/js',
-    image: 'assets/images',
-    font: 'assets/fonts',
-    vendor: 'assets/vendors',
-  };
-}());
+function css() {
+  return src(scrPath.less.main)
+      .pipe(less())
+      .pipe(concat(pkg.name + '.min.css'))
+      .pipe(dest(distPath.css));
+}
 
-var src = (function () {
-  return {
-    sass: {
-      main: 'src/scss/app.scss',
-      files: ['src/scss/**/**']
-    },
-    css: {
-      main: []
-    },
-    js: {
-      main: ['src/js/common.js']
-    },
-    font: {
-      main: 'src/fonts/**/**'
-    },
-    vendor: {
-      main: 'src/vendors/**/**'
-    }
-  };
-}());
+function js() {
+  return src(scrPath.js.main)
+  .pipe(concat(pkg.name + '.min.js'))
+  .pipe(uglify())
+  .pipe(dest(distPath.js));
+}
 
-var banner = (function () {
-  return ['/**',
-    ' * <%= pkg.name %> - <%= pkg.description %>',
-    ' * @version <%= pkg.version %>',
-    ' * @link    <%= pkg.homepage %>',
-    ' * @author  <%= pkg.author.name %> (<%= pkg.author.url %>)',
-    ' * @license <%= pkg.license %>',
-    ' */'
-  ].join('\n');
-}());
+function vendor() {
+  return src(scrPath.vendor.main)
+  .pipe(dest(distPath.vendor));
+}
 
-//# tasks
-gulp.task('css', function () {
-  gulp.src(src.css.main)
-    .pipe(plumber())
-    .pipe(addSrc(src.sass.main))
-    .pipe(sass({
-      outputStyle: 'compressed'
-    }))
-    .pipe(prefix())
-    .pipe(concat(pkg.name + '.min.css'))
-    .pipe(header(banner, {
-      pkg: pkg
-    }))
-    .pipe(gulp.dest(dist.css));
-});
+function font() {
+  return src(scrPath.font.main)
+  .pipe(dest(distPath.font));
+}
 
-gulp.task('js', function () {
-  gulp.src(src.js.main)
-    .pipe(plumber())
-    .pipe(concat(pkg.name + '.min.js'))
-    .pipe(uglify())
-    .pipe(header(banner, {
-      pkg: pkg
-    }))
-    .pipe(gulp.dest(dist.js));
-});
 
-gulp.task('vendor', function () {
-  gulp.src(src.vendor.main)
-    .pipe(gulp.dest(dist.vendor));
-});
-
-gulp.task('font', function () {
-  gulp.src(src.font.main)
-    .pipe(gulp.dest(dist.font));
-});
-
-gulp.task('server', function () {
-  browserSync.init({
-    proxy: 'http://127.0.0.1:#{PORT.GHOST}'
-  });
-});
-
-gulp.task('zip', ['css', 'js', 'vendor', 'font'], function () {
-  var targetDir = 'dist/';
-  var themeName = require('./package.json').name;
-  var filename = themeName + '.zip';
-
-  return gulp.src([
-      '**',
-      '!node_modules',
-      '!node_modules/**',
-      '!dist',
-      '!dist/**',
-      '!src',
-      '!src/**',
-  ])
-      .pipe(zip(filename))
-      .pipe(gulp.dest(targetDir));
-});
-
-gulp.task('build', ['css', 'js', 'vendor', 'font']);
-
-gulp.task('default', function () {
-  gulp.start(['build']);
-  gulp.watch(src.js.main, ['js']);
-  gulp.watch(src.sass.files, ['css']);
-  gulp.watch(src.font.main, ['font']);
-  gulp.watch(src.vendor.main, ['vendor']);
-});
+exports.build = series(css, js, vendor, font);
